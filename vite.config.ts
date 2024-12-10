7de62661bb8f929dbf7fb32f5f4344d20b5737c5
Khalilkadri98 <khalilkadri023@gmail.com>
2024-12-10 20:45:39 +0100

    project deployed

--- vite.config.ts
+++ vite.config.ts
@@ -11,11 +11,12 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: mode === "production" ? "/<portfolio.github.io>/" : "/",
 }));
